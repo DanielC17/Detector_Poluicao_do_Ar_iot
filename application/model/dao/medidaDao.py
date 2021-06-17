7102739fd4560f52cfb53845c0e54cb32d00eab2
Daniel Cabral <danielccabral1981@gmail.com>
2021-06-16 23:49:20 -0300

    criando def para appendar no post

--- application/model/dao/medidaDao.py
+++ application/model/dao/medidaDao.py
@@ -9,10 +9,15 @@ class MedidaDao():
     def listarMedicoes(self):
         return self._medicoes
 
+    def adicionarMedicoes(self, medicao):
+        self._medicoes.append(medicao)
+
 
 medicao_1 = Medida(1, 1, 1, 1, 1, "14/06/2021")
 medicao_2 = Medida(1, 2, 2, 2 ,2, "15/06/2021")
 medicao_3 = Medida(2, 3, 3, 3, 3, "16/06/2021")
 
 medida_list = MedidaDao([medicao_1, medicao_2,medicao_3])
+
+
 
