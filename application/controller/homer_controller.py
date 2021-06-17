0c91672d0d741d90875c7dacfa8477a19cf1d5f7
Daniel Cabral <danielccabral1981@gmail.com>
2021-06-16 23:49:35 -0300

    route para id e data

--- application/controller/homer_controller.py
+++ application/controller/homer_controller.py
@@ -19,7 +19,27 @@ def adicionarMedicoes():
     materialparticulado = float(request.json.get("materialparticulado", None))
     monoxido_carbono = float(request.json.get("monoxido_carbono", None))
     oxido_nitroso = float(request.json.get("oxido_nitroso", None))
-    data = float(request.json.get("data", None))
+    data = request.json.get("data", None)
+
+    medida = Medida(id, ozonio, materialparticulado, monoxido_carbono, oxido_nitroso, data)
+    medida_list.adicionarMedicoes(medida)
+    return medida.toDict()
 
 
+@app.route("/medicoes/id/<id>", methods=['GET'])
+def buscarIdMedicoes(id: int):
+    medida_list_dict = []
+    for i in medida_list.listarMedicoes():
+        if int(i.get_id()) == int(id):
+            medida_list_dict.append(i.toDict())
+    return jsonify(medida_list_dict)
 
+
+@app.route("/medicoes/data/<data>", methods=['GET'])
+def buscarDataMedicoes(data):
+    medida_list_dict = []
+    for i in medida_list.listarMedicoes():
+        date = i.get_data().replace("/", "")
+        if date == data:
+            medida_list_dict.append(i.toDict())
+    return jsonify(medida_list_dict)
